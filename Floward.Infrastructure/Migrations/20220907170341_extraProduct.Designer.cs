46f14940be795d60bc06ad9aa840e532d4423ac1
TChukwuleta <tobechichukwuleta125@gmail.com>
2022-09-07 18:11:02 +0100

    update

--- Floward.Infrastructure/Migrations/20220907170341_extraProduct.Designer.cs
+++ Floward.Infrastructure/Migrations/20220907170341_extraProduct.Designer.cs
@@ -4,6 +4,7 @@ using Floward.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Floward.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220907170341_extraProduct")]
+    partial class extraProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
